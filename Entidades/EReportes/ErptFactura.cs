913eacaa27d85c56ae57f18fdcb1d63695ac79ab
Carlos J Pacheco <Carlosj.pacheco15@gmail.com>
2022-05-11 17:07:42 -0400

    rporte factura

---
+++ Entidades/EReportes/ErptFactura.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Entidades.EReportes
+{
+    public class ErptFactura
+    {
+        public int IdFactura { get; set; }
+        public int IdCliente { get; set; }
+        public string IdComprobante { get; set; }
+        public DateTime FechaFactura { get; set; }
+        public string TipoCompra { get; set; }
+        public string NotaFactura { get; set; }
+        public decimal ImporteFactura { get; set; }
+        public decimal DescuentoFactura { get; set; }
+        public decimal ItbisFactura { get; set; }
+        public decimal Total { get; set; }
+        public string NombreCliente { get; set; }
+        public string CedulaCliente { get; set; }
+        public string RncCliente { get; set; }
+        public string DireccionCliente { get; set; }
+        public string TelefonoCliente { get; set; }
+        public string CorreoCliente { get; set; }
+        public string NcfComprobante { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public string NombreComprobante { get; set; }
+    }
+}
