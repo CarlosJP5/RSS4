81051fb4013cb8d1c9b45bb148f4c989d23315f4
Carlos J Pacheco <Carlosj.pacheco15@gmail.com>
2022-09-06 17:53:44 -0400

    Login Complete

--- APP/FrmLogin.Designer.cs
+++ APP/FrmLogin.Designer.cs
@@ -29,20 +29,21 @@
         private void InitializeComponent()
         {
             this.panelLeft = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelRight = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.linkOlvideClave = new System.Windows.Forms.LinkLabel();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.txtClave = new System.Windows.Forms.TextBox();
             this.panelUsuarioUnderLine = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
             this.txtUsuario = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panelLeft.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panelRight.SuspendLayout();
@@ -51,7 +52,7 @@
             // panelLeft
             // 
             this.panelLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(235)))), ((int)(((byte)(182)))));
-            this.panelLeft.Controls.Add(this.button1);
+            this.panelLeft.Controls.Add(this.button3);
             this.panelLeft.Controls.Add(this.label2);
             this.panelLeft.Controls.Add(this.label1);
             this.panelLeft.Controls.Add(this.pictureBox1);
@@ -61,15 +62,16 @@
             this.panelLeft.Size = new System.Drawing.Size(300, 450);
             this.panelLeft.TabIndex = 1;
             // 
-            // label1
+            // button3
             // 
-            this.label1.Font = new System.Drawing.Font("Elephant", 39.74999F);
-            this.label1.Location = new System.Drawing.Point(0, 190);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(300, 60);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "RSS";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = global::APP.Properties.Resources.conexion_24;
+            this.button3.Location = new System.Drawing.Point(10, 410);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 25);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
@@ -81,16 +83,15 @@
             this.label2.Text = "Retail Sales System";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // button1
+            // label1
             // 
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Image = global::APP.Properties.Resources.conexion_24;
-            this.button1.Location = new System.Drawing.Point(10, 410);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(30, 25);
-            this.button1.TabIndex = 3;
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.Font = new System.Drawing.Font("Elephant", 39.74999F);
+            this.label1.Location = new System.Drawing.Point(0, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 60);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RSS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // pictureBox1
             // 
@@ -104,8 +105,9 @@
             // 
             // panelRight
             // 
-            this.panelRight.Controls.Add(this.button3);
-            this.panelRight.Controls.Add(this.button2);
+            this.panelRight.Controls.Add(this.linkOlvideClave);
+            this.panelRight.Controls.Add(this.btnSalir);
+            this.panelRight.Controls.Add(this.btnIniciar);
             this.panelRight.Controls.Add(this.panel1);
             this.panelRight.Controls.Add(this.txtClave);
             this.panelRight.Controls.Add(this.panelUsuarioUnderLine);
@@ -114,30 +116,54 @@
             this.panelRight.Controls.Add(this.label4);
             this.panelRight.Controls.Add(this.label3);
             this.panelRight.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelRight.Font = new System.Drawing.Font("Century Schoolbook", 20.25F, System.Drawing.FontStyle.Bold);
             this.panelRight.Location = new System.Drawing.Point(300, 0);
             this.panelRight.Name = "panelRight";
             this.panelRight.Size = new System.Drawing.Size(400, 450);
             this.panelRight.TabIndex = 0;
             // 
-            // label3
+            // linkOlvideClave
             // 
-            this.label3.Location = new System.Drawing.Point(0, 60);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(400, 30);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Iniciar Sesión";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.linkOlvideClave.AutoSize = true;
+            this.linkOlvideClave.Location = new System.Drawing.Point(236, 264);
+            this.linkOlvideClave.Name = "linkOlvideClave";
+            this.linkOlvideClave.Size = new System.Drawing.Size(114, 13);
+            this.linkOlvideClave.TabIndex = 9;
+            this.linkOlvideClave.TabStop = true;
+            this.linkOlvideClave.Text = "Olvidaste Contraseña?";
             // 
-            // label4
+            // btnSalir
             // 
-            this.label4.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(46, 130);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 20);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Usuario";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(235)))), ((int)(((byte)(182)))));
+            this.btnSalir.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(105)))), ((int)(((byte)(83)))));
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Century Schoolbook", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Image = global::APP.Properties.Resources.Salir_16;
+            this.btnSalir.Location = new System.Drawing.Point(50, 360);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(300, 35);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSalir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(235)))), ((int)(((byte)(182)))));
+            this.btnIniciar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(105)))), ((int)(((byte)(83)))));
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciar.Font = new System.Drawing.Font("Century Schoolbook", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Image = global::APP.Properties.Resources.Aceptar_16;
+            this.btnIniciar.Location = new System.Drawing.Point(50, 302);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(300, 35);
+            this.btnIniciar.TabIndex = 7;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnIniciar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
             // 
             // panel1
             // 
@@ -145,21 +171,21 @@
             this.panel1.Location = new System.Drawing.Point(50, 259);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(300, 1);
-            this.panel1.TabIndex = 11;
+            this.panel1.TabIndex = 6;
             // 
             // txtClave
             // 
             this.txtClave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
             this.txtClave.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtClave.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
             this.txtClave.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtClave.Location = new System.Drawing.Point(50, 239);
             this.txtClave.MaxLength = 50;
             this.txtClave.Name = "txtClave";
             this.txtClave.Size = new System.Drawing.Size(300, 20);
-            this.txtClave.TabIndex = 10;
+            this.txtClave.TabIndex = 5;
             this.txtClave.Text = "1654";
             this.txtClave.UseSystemPasswordChar = true;
+            this.txtClave.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtClave_KeyDown);
             // 
             // panelUsuarioUnderLine
             // 
@@ -167,7 +193,7 @@
             this.panelUsuarioUnderLine.Location = new System.Drawing.Point(50, 180);
             this.panelUsuarioUnderLine.Name = "panelUsuarioUnderLine";
             this.panelUsuarioUnderLine.Size = new System.Drawing.Size(300, 1);
-            this.panelUsuarioUnderLine.TabIndex = 8;
+            this.panelUsuarioUnderLine.TabIndex = 3;
             // 
             // label5
             // 
@@ -175,7 +201,7 @@
             this.label5.Location = new System.Drawing.Point(46, 209);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(106, 20);
-            this.label5.TabIndex = 9;
+            this.label5.TabIndex = 4;
             this.label5.Text = "Contraseña";
             this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
@@ -189,40 +215,29 @@
             this.txtUsuario.MaxLength = 50;
             this.txtUsuario.Name = "txtUsuario";
             this.txtUsuario.Size = new System.Drawing.Size(300, 20);
-            this.txtUsuario.TabIndex = 7;
+            this.txtUsuario.TabIndex = 2;
             this.txtUsuario.Text = "CHE";
+            this.txtUsuario.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUsuario_KeyDown);
             // 
-            // button2
+            // label4
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(235)))), ((int)(((byte)(182)))));
-            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(105)))), ((int)(((byte)(83)))));
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Century Schoolbook", 14.25F);
-            this.button2.Image = global::APP.Properties.Resources.Aceptar_16;
-            this.button2.Location = new System.Drawing.Point(50, 302);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(300, 35);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "Iniciar";
-            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button2.UseVisualStyleBackColor = false;
+            this.label4.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(46, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Usuario";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // button3
+            // label3
             // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(235)))), ((int)(((byte)(182)))));
-            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(105)))), ((int)(((byte)(83)))));
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Century Schoolbook", 14.25F);
-            this.button3.Image = global::APP.Properties.Resources.Salir_16;
-            this.button3.Location = new System.Drawing.Point(50, 360);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(300, 35);
-            this.button3.TabIndex = 13;
-            this.button3.Text = "Salir";
-            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button3.UseVisualStyleBackColor = false;
+            this.label3.Font = new System.Drawing.Font("Century Schoolbook", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(0, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(400, 30);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Iniciar Sesión";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // FrmLogin
             // 
@@ -247,19 +262,20 @@
         #endregion
 
         private System.Windows.Forms.Panel panelLeft;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panelRight;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox txtClave;
         private System.Windows.Forms.Panel panelUsuarioUnderLine;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtUsuario;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.LinkLabel linkOlvideClave;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button button3;
     }
 }
--- APP/FrmLogin.cs
+++ APP/FrmLogin.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using Negocios;
+using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace APP
@@ -15,6 +10,51 @@ namespace APP
         public FrmLogin()
         {
             InitializeComponent();
+        }
+
+        private readonly NUsuario nUsuario = new NUsuario();
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            DataTable usuario = nUsuario.Login(txtUsuario.Text);
+            if (usuario.Rows.Count > 0)
+            {
+                if (txtClave.Text == usuario.Rows[0][8].ToString())
+                {
+                    DialogResult = DialogResult.OK;
+                }
+                else
+                {
+                    MessageBox.Show("Clave Incorrecta", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Usuario Incorrecto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtUsuario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                txtClave.Focus();
+            }
+        }
+
+        private void txtClave_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                btnIniciar.PerformClick();
+            }
         }
     }
 }
