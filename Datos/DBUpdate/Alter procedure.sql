431714178997348b8e7be633e8625dfcbd8942ac
Carlos J Pacheco <Carlosj.pacheco15@gmail.com>
2023-12-14 16:28:45 -0400

    db

---
+++ Datos/DBUpdate/Alter procedure.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Caja
+(
+	id_caja int primary key,
+	apertura_caja datetime,
+	apertura_nombre varchar(50),
+	cierre_caja datetime,
+	cierre_nombre varchar(50),
+	total_caja float
+)
+GO
