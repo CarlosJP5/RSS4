8e6d82e453600bc373b69e94100336b54df311e2
Carlos J Pacheco <Carlosj.pacheco15@gmail.com>
2023-12-21 12:05:11 -0400

    232

--- APP/zPrincipal.Designer.cs
+++ APP/zPrincipal.Designer.cs
@@ -250,6 +250,7 @@ namespace APP
             this.cajaToolStripMenuItem.Name = "cajaToolStripMenuItem";
             this.cajaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.cajaToolStripMenuItem.Text = "Caja";
+            this.cajaToolStripMenuItem.Visible = false;
             // 
             // aperturaDeCajaToolStripMenuItem
             // 
