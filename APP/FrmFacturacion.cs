0df99795f770563381a33dd054cbd9d611e828ec
Carlos J. Pacheco <Carlos_Jp11@hotmail.com>
2024-01-04 20:44:49 -0400

    Aqui?

--- APP/FrmFacturacion.cs
+++ APP/FrmFacturacion.cs
@@ -340,7 +340,7 @@ namespace APP
                 e.Handled = true;
             }
 
-            // only allow one decimal point
+            // only allow one decimal po
             if (e.KeyChar == '.' && (sender as TextBox).Text.IndexOf('.') > -1)
             {
                 e.Handled = true;
