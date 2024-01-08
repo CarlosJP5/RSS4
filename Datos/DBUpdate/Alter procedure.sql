387ad84a3a1649c8c717b0a408a0857b66e2510a
Carlos J Pacheco <Carlosj.pacheco15@gmail.com>
2024-01-08 10:06:29 -0400

    lic

--- Datos/DBUpdate/Alter procedure.sql
+++ Datos/DBUpdate/Alter procedure.sql
@@ -66,3 +66,39 @@ left join ComprobantesDetalle cd on d.id_comprobante = cd.id_comprobante and CON
 WHERE d.fecha_devolucion between @desde and @hasta
 end
 go
+
+select * into temp from Empresa
+
+/****** Object:  Table [dbo].[Empresa]    Script Date: 1/8/2024 9:50:55 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Empresa]') AND type in (N'U'))
+DROP TABLE [dbo].[Empresa]
+GO
+
+/****** Object:  Table [dbo].[Empresa]    Script Date: 1/8/2024 9:50:55 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Empresa](
+	[id_empresa] [int] NOT NULL,
+	[nombre_empresa] [varchar](50) NULL,
+	[direccion_empresa] [varchar](150) NULL,
+	[rnc_empresa] [varchar](15) NULL,
+	[telefono] [varchar](50) NULL,
+	[cell_empresa] [varchar](20) NULL,
+	[correo_empresa] [varchar](50) NULL,
+	[lisencia_empresa] [date] NOT NULL,
+ CONSTRAINT [PK__Empresa__4A0B7E2C1FBB3FFE] PRIMARY KEY CLUSTERED 
+(
+	[id_empresa] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+insert into Empresa select *,'12/31/2040' from temp
+go
+
+drop table temp
+go
