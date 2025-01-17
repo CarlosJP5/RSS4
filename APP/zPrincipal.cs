44b13076d1a96f5141218e1a50d09e2ff13a33fc
Carlos J. Pacheco <Carlos J. Pacheco@CHELT>
2025-01-17 11:02:19 -0400

    Licencia update

--- APP/zPrincipal.cs
+++ APP/zPrincipal.cs
@@ -22,7 +22,7 @@ namespace APP
 
         private void zPrincipal_Load(object sender, EventArgs e)
         {
-            DateTime licenciaLock = DateTime.Parse("01/10/2025");
+            DateTime licenciaLock = DateTime.Parse("06/14/2025");
             NrptEmpresa lic = new NrptEmpresa();
             DateTime lisencia = lic.lisencia();
             if (licenciaLock != lisencia)
