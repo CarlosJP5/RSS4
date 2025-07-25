ab4d62dc657a2de2146d7fdb787c111f99f02522
Carlos J. Pacheco <Carlos J. Pacheco@CHELT>
2025-07-25 12:55:47 -0400

    fecha

--- APP/zPrincipal.cs
+++ APP/zPrincipal.cs
@@ -26,7 +26,7 @@ namespace APP
 
         private void zPrincipal_Load(object sender, EventArgs e)
         {
-            DateTime licenciaLock = new DateTime(2025, 12, 31);
+            DateTime licenciaLock = new DateTime(2025, 12, 28);
             NrptEmpresa lic = new NrptEmpresa();
             DateTime lisencia = lic.lisencia();
             if (licenciaLock != lisencia)
